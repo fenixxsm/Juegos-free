13188eb2cab69159de51939e6a1e6c3e38dde1a1
fenixxsm <163020297+fenixxsm@users.noreply.github.com>
2024-12-08 01:02:20 -0600

    beta 1.0.6.3

--- fondo.js
+++ fondo.js
@@ -31,7 +31,7 @@ function changeColors() {
 }
 // Guardar el diccionario en localStorage si no está almacenado
 const diccionarioPorDefecto = {
-    "version": "   Beta 1.0.6.2",
+    "version": "   Beta 1.0.6.3",
 };
 
 if (!localStorage.getItem('diccionarioIds')) {
@@ -67,7 +67,7 @@ function actualizarDiccionario(nuevoDiccionario) {
 
 // Ejemplo: Actualizar el diccionario
 const nuevoDiccionario = {
-    "version": "   Beta 1.0.6.2",
+    "version": "   Beta 1.0.6.3",
 };
 actualizarDiccionario(nuevoDiccionario);
 
